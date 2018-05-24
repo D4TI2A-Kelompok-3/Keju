e8b3ac9997119042b1e57ddb6c513003a2b17642
faridalwan <muhammadfaridalwana@gmail.com>
2018-05-25 01:01:16 +0700

    update kodingan

--- input/coba.py
+++ input/coba.py
@@ -1,10 +1,7 @@
 from flask import Flask,request,jsonify,json
 app = Flask(__name__)
-
 vars = [{'ttl':'01 Feb 2018'}]
-
 def coba(a):
     var = {'ttl' : request.json['ttl']}
-
     vars.append(var)
-    return jsonify({'vars' : vars})
+    return jsonify({'vars':vars})
