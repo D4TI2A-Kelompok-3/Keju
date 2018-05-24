7d1c888e60cb8f31776a79278525e45cbccf2031
Rojasqifadilla <rojasqi.fadilla13@gmail.com>
2018-05-24 23:42:29 +0700

    cobajsonflask

--- input/main.py
+++ input/main.py
@@ -2,9 +2,17 @@ from flask import Flask
 from flask import request
 app = Flask(__name__)
 
-@app.route('/postjson', methods=['POST'])
-def postjson():
-	print (request.is_json)
-	content = request.get_json()
-    print (content)
-    return 'JSON posted'
+# If /json route receives header "application/json"
+@app.route('/json', methods=['GET','POST'])
+def json():
+
+	  app.logger.debug("JSON received...")
+      app.logger.debug(request.json)
+    
+    if request.json:
+        mydata = request.json
+        
+        return "mengembalikan json tempat tanggal_lahir %s" % mydata.get("ttl")
+
+    else:
+        return "no json received"
